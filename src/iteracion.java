d6dd63f63d5748b609b5392c642c5159998eb9f8
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2023-05-26 03:24:50 -0500

    iteracion java  0 - 1000000000

---
+++ src/iteracion.java
@@ -0,0 +1,16 @@
+public class iteracion {
+	public static final long STOP = 1000000000000L;
+	
+	public static void main (String []args) {
+		long t1 = System.currentTimeMillis();
+		for (long i = 0; i < STOP; i++){
+			if (i % 1000000000 == 0){
+				System.out.println(i + " - " + (System.currentTimeMillis() - t1));				
+			}
+		}
+		long t2 = System.currentTimeMillis() -t1;
+	
+		System.out.println("la funcion tardo " + t2 / 1000.0 + " segundos en ejecutarse.");
+	
+	}
+}
