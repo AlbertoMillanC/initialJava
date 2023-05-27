d586dd7012f83d16e1f4904058cc4516bdff739c
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2023-05-27 01:13:43 -0500

    if age is > 18 welcome

---
+++ src/EdadMayor.java
@@ -0,0 +1,18 @@
+public class EdadMayor {
+
+        public static void main(String[] args) {
+            System.out.println("probando condicionales");
+    
+            int edad = 20;
+    
+            if (edad >= 18) {
+                System.out.println("Tienes más de 18 años");
+                System.out.println("Bienvenido");
+            } else {
+                System.out.println("Lamentablemente no puedes ingresar");
+            }
+        }
+    
+    
+    
+}
